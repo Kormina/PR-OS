36e8416affe8d4a4977af6cec1700d86498ffe53
kormina <kormina01@mail.ru>
2023-01-10 16:47:44 +0500

    э

---
+++ heat/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using heat.Data;
+using heat.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using HeatClassLibrary;
+
+namespace heat.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ heat/Models/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using heat.Data;
+
+namespace heat.Models
+{
+        public class HomeIndexViewModel
+        {
+            public Variant? Variant { get; set; }
+            public List<Variant> Variants { get; set; }
+        }
+    }
